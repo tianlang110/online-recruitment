f84d282acf32ebf55b8ad1b5c7ae4b8ea9454927
tianlang <1104569851@qq.com>
2020-11-13 14:41:08 +0800

    commit all local code to origin

---
+++ src/main/java/com/longlong/OnlineRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.longlong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineRecruitmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longlong/OnlineRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longlong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
