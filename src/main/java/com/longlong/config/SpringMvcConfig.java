39ca328e8bd9c83db1d845868a0c35a278b0148a
tianlang <1104569851@qq.com>
2020-11-17 11:06:40 +0800

    1

---
+++ src/main/java/com/longlong/config/SpringMvcConfig.java
@@ -0,0 +1,17 @@
+package com.longlong.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/login").setViewName("user/login");
+        registry.addViewController("/reg").setViewName("user/reg");
+    }
+}
